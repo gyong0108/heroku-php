068685434b4cd3ecabd20392559356947f5fd104
Kashirin Alexandra <39225927+gyong0108@users.noreply.github.com>
2020-02-16 00:11:26 +1000

    Heroku2

--- web/admin/db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conn = mysql_connect("localhost","root","");
-$db_select = mysql_select_db("healthylife");
-mysql_set_charset("utf8", $conn);
-
-?>
--- web/admin/deleteProduct.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("db_connect.php");
-$id = $_POST['id'];
-
-$query = "DELETE from products where id='".$id."'";
-$result = mysql_query($query);
-
-?>
